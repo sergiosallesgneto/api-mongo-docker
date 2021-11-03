5cd8462f9a187b23d73261ae5fb533f2acbe59fe
Claudio Nunes <claudio@metasistemas.com>
2021-11-03 15:32:19 -0300

    Suporte mongodb

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const app = express();
+const {MongoClient} = require("mongodb");
 
-app.get("/aluno", (req, res )=> {
-    let usuario = {
-                    nome: "Claudio Nunes",
-                    disciplina : "Topicos Especiais",
-                    curso : "ADS"
-    }
-    res.send(usuario)
+
+app.get("/alunos", (req, res )=> {
+    const cliente = new  MongoClient("mongodb://192.168.100.100:27017");
+    cliente.connect();
+    cliente.db("Fatec").collection("Alunos").find()
+    .toArray().then( (alunos) => {
+        res.send(alunos)
+    }    )
+  
 }) ;
 
-app.get("/professores", (req, res )=> {
-    let professores  = [ {
-                    nome: "David Reis",
-                    disciplina : "Topicos Especiais",
-                    curso : "ADS"
-    },
-    {
-        nome: "Alexandre Oliveira Garcia ",
-        disciplina : "O danado do HASKELL",
-        curso : "ADS"
-}]
-    res.send(professores);
-}) ;
+
 
 
 app.listen(3000, () => {
